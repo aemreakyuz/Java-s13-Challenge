f51bb0c53e7e1e21edc15f371c6dac4f6e4f1e08
Emre Akyüz <aemreakyuz@gmail.com>
2023-11-19 22:40:00 +0300

    OOP, Encapsulation

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -1,29 +1,49 @@
 package employeeApp;
 
-/**
- * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
- *
- */
+import employeeApp.enums.Plan;
+import employeeApp.model.Company;
+import employeeApp.model.Employee;
+import employeeApp.model.Healthplan;
+
+import java.util.Arrays;
+
 public class Main
 {
-
-    /**
-     * Uygulamadaki tüm işlemler bu metotda yapılmalı
-     */
     private static void workWithData ()
     {
+        Healthplan plan1 = new Healthplan(1, "A Sigorta", Plan.ADVANCE);
+        Healthplan plan2 = new Healthplan(2, "B Sigorta", Plan.MEDIUM);
+
+        System.out.println(plan1);
+        System.out.println(plan2);
+
+        String[] healthplans = new String[3];
+        healthplans[0] = plan1.getName();
+        healthplans[1] = plan2.getName();
+
+        Employee employee = new Employee(1, "Emre",
+                "emre.com", "1234", healthplans);
+
+        employee.addHealthplan(0, "C Sigorta");
+        employee.addHealthplan(2, "A Sigorta");
+        employee.addHealthplan(2, "C Sigorta");
+        employee.addHealthplan(3, "T Sigorta");
+        System.out.println(Arrays.toString(employee.getHealthPlans()));
+
+        System.out.println("**********************************");
+        Company company = new Company(1, "Workintech",
+                1000000000, new String[]{"Emre", null, null});
+
+        company.addEmployee(0, "Ali ");
+        company.addEmployee(2, "Emre");
+        company.addEmployee(-1, "Emre");
+        company.addEmployee(5, "Emre");
+
+        System.out.println(Arrays.toString(company.getDeveloperNames()));
+        System.out.println(company);
 
     }
 
-    /**
-     * Bu projenin ana(main) metodu. Java uygulamalarında main metot küçük bir metot olur.
-     * Bir Java programınd ilgili işlemlerin çoğunluğu başka metodlarda yapılır.
-     * Daha sonra ana metod diğer metodları çağırır. Böylece ana metodun içi kalabalık bir hale gelmemiş olur.
-
-     *
-     * @param args bu uygulamada kullanılmamaktadır. Command Line üzerinden argüman alan
-     *             bir uygulamamız olsaydı kullanılacaktı.
-     */
     public static void main(String[] args)
     {
         workWithData();
